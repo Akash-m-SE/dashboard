ea9a9edd1f2c22e06f352e7d71bf0c99002a69a4
Akash-m-SE <akashm9923@gmail.com>
2024-02-10 22:00:00 +0530

    updated syncfusion registration license key and stacked component

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -22,25 +22,24 @@ const Stacked = ({ width, height }) => {
   const { currentMode } = useStateContext();
 
   return (
-    // <ChartComponent
-    //   id="charts"
-    //   primaryXAxis={stackedPrimaryXAxis}
-    //   primaryYAxis={stackedPrimaryYAxis}
-    //   width={width}
-    //   height={height}
-    //   chartArea={{ border: { width: 0 } }}
-    //   tooltip={{ enable: true }}
-    //   background={currentMode === "Dark" ? "#33373E" : "#fff"}
-    //   legendSettings={{ background: "white" }}
-    // >
-    //   <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
-    //   <SeriesCollectionDirective>
-    //     {stackedCustomSeries.map((item, index) => (
-    //       <SeriesDirective key={index} {...item} />
-    //     ))}
-    //   </SeriesCollectionDirective>
-    // </ChartComponent>
-    <ChartComponent></ChartComponent>
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ContextProvider } from "./context/ContextProvider.jsx";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
